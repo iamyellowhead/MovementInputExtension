35bbcc00c02b56a0958d3e08dc91fc2dad3ad500
Y3LL0W <Iamyellowhead@gmail.com>
2025-08-18 20:53:31 +0200

    updated 164

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 group = "me.yellowhead"
-version = "0.9.0-beta-163"
+version = "0.9.0-beta-164"
 
 plugins {
-    kotlin("jvm") version "2.0.21"
-    id("com.typewritermc.module-plugin") version "1.3.0"
+    kotlin("jvm") version "2.2.10"
+    id("com.typewritermc.module-plugin") version "2.0.0"
     id("com.google.devtools.ksp") version "2.0.21-1.0.28"
 }
 
@@ -14,11 +14,10 @@ typewriter {
         name = "PlayerInputAudiences"
         shortDescription = "Detect and filter players based on key inputs"
         description = "Provides audience filters for detecting player inputs in real-time, including movement (W/A/S/D) and actions (jump, sneak, sprint).Useful for custom mechanics, AFK detection, or gameplay triggers."
-        engineVersion = "0.9.0-beta-163"
+        engineVersion = "0.9.0-beta-164"
         channel = com.typewritermc.moduleplugin.ReleaseChannel.BETA
 
         paper {
-            // You can put Paper-specific config here if needed
         }
     }
 }
